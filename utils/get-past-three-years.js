826181076e10e17c0f564ab41e9f83be9f9e4880
Jen Weber <j@jenweber.me>
2017-12-06 22:04:43 -0500

    fix date handling and add GitHub username arg

--- utils/get-past-three-years.js
+++ utils/get-past-three-years.js
@@ -2,26 +2,33 @@ const requestData = require('./request-data')
 const moment = require('moment')
 
 const startingMonth = '2015-01'
-const endingMonth = '2017-11'
+const endingMonth = '2015-02'
 
+
+const user = process.argv[2]
+if (!user) {
+  console.error('ERROR: to run this script, you must provide your GitHub username as an argument, i.e. "node get-past-three-years yourname"')
+  return
+}
 
 function fetchFiles() {
   // months are zero indexed!!! wtf
   let start = moment(startingMonth, "YYYY-MM")
-  let endExcluding = moment(endingMonth, "YYYY-MM")
+  let endExcluding = moment(endingMonth, "YYYY-MM").add(1, 'months')
   let current = start
-  while (current !== endExcluding) {
-    console.log('current', current)
-    console.log('end', endExcluding)
+  let counter = 0
+  while (current.format('YYYY-MM') !== endExcluding.format('YYYY-MM')) {
     let query = calcDateRange(current)
-    requestData(query)
+    requestData(query, user)
     current = current.add(1, 'months')
+    counter += 1
   }
 }
 
 function calcDateRange(momentDate) {
   let daysInMonth = momentDate.daysInMonth()
-  let month = momentDate.month()
+  let month = momentDate.month() + 1
+  month = month < 10 ? '0' + String(month) : month
   let year = momentDate.year()
   let start = `${year}-${month}-01`
   let end = `${year}-${month}-${daysInMonth}`
--- utils/request-data.js
+++ utils/request-data.js
@@ -1,24 +1,27 @@
 'use strict'
 
+const fs = require('fs')
 const request = require('request');
 
-const headers = {
-    'Accept': 'application/vnd.github.mercy-preview+json'
-};
-
 function handleResponse(error, response, body) {
+  // console.log(response)
+  let endDate = response.req.path.split(':')[1].split('..')[0]
+  console.log(endDate)
     if (!error && response.statusCode == 200) {
-        console.log(body);
+        // console.log(body);
     }
 }
 
-function requestData(dateRange) {
+function requestData(dateRange, user) {
   let options = {
       url: `https://api.github.com/search/repositories?q=ember+created:${dateRange}`,
-      headers: headers
+      headers: {
+          'Accept': 'application/vnd.github.mercy-preview+json',
+          'User-Agent': user
+      }
   };
-  console.log(options.url)
-  // request(options, handleResponse);
+  // console.log(options.url)
+  request(options, handleResponse);
 }
 
 module.exports = requestData;
