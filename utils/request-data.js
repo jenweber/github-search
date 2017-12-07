7d0baa2bfb6b3517138a4bd71bc2cd0cde4e8978
Jen Weber <j@jenweber.me>
2017-12-06 22:20:25 -0500

    write response to file

--- utils/request-data.js
+++ utils/request-data.js
@@ -6,9 +6,13 @@ const request = require('request');
 function handleResponse(error, response, body) {
   // console.log(response)
   let endDate = response.req.path.split(':')[1].split('..')[0]
-  console.log(endDate)
     if (!error && response.statusCode == 200) {
-        // console.log(body);
+        fs.writeFile(`./data/${endDate}.json`, body, 'utf8', function (err) {
+          if (err) {
+              return console.log(err);
+          }
+          console.log(endDate + '.json' + ' saved');
+      });
     }
 }
 
