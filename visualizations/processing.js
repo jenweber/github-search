22b08543f70431eff5d036359c5d3ab468ac4bab
Jen Weber <j@jenweber.me>
2017-12-06 23:58:40 -0500

    charting organization repos

--- visualizations/processing.js
+++ visualizations/processing.js
@@ -1,15 +1,57 @@
 const folder = './data/';
 const fs = require('fs');
 
-let data = []
+function rawCount() {
+  let data = []
 
-fs.readdirSync(folder).forEach(file => {
-  let json = fs.readFileSync(`./data/${file}`)
-  let obj = JSON.parse(json)
-  let date = file.split('.')[0]
-  let incompleteResults = obj.incomplete_results
-  let totalCount = obj.total_count
-  data.push([ date, totalCount, incompleteResults])
-})
+  fs.readdirSync(folder).forEach(file => {
+    let json = fs.readFileSync(`./data/${file}`)
+    let obj = JSON.parse(json)
+    let date = file.split('.')[0]
+    let incompleteResults = obj.incomplete_results
+    let totalCount = obj.total_count
 
-console.log(data)
+    if (!incompleteResults) {
+      console.log(`WARNING ${file} results are incomplete`)
+    }
+
+    data.push([ date, totalCount ])
+  })
+
+  console.log('RAW COUNT', data)
+}
+
+function orgCount() {
+  let data = []
+
+  fs.readdirSync(folder).forEach(file => {
+    let json = fs.readFileSync(`./data/${file}`)
+    let obj = JSON.parse(json)
+    let date = file.split('.')[0]
+    let incompleteResults = obj.incomplete_results
+    let repos = obj.items
+
+    if (!incompleteResults) {
+      console.log(`WARNING ${file} results are incomplete`)
+    }
+
+    totalOrgCount = countReposForOrgs(repos)
+
+    data.push([ date, totalOrgCount ])
+  })
+
+  console.log('ORGANIZATION COUNT', data)
+}
+
+function countReposForOrgs(repos) {
+  let counter = 0;
+  repos.forEach(function(repo) {
+    if (repo.owner.type === "Organization") {
+      counter += 1
+    }
+  })
+  return counter
+}
+
+// rawCount()
+orgCount()
